5aa12afcd2bbe5fcabf7001f275a9f3cbb622aa2
Sean Thomas <columbuscoder@gmail.com>
2020-04-30 15:21:17 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import groceries from './order-details';
 
+
+// a good component would be to display the thumbnail image
+const images = groceries.products.map((product) => product.images);
+const thumbnails = images.forEach(array => {
+
+  console.log(array.filter(item => item.size === 'thumbnail'))
+});
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {thumbnails}
     </div>
   );
 }
